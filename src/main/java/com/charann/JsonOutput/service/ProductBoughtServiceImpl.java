2873e62532d06f056a4107acf43545b3d6773afe
CharannSdsConnect <sdsuser010@gmail.com>
2024-09-04 16:39:33 +0530

    Update ProductBoughtServiceImpl.java

--- src/main/java/com/charann/JsonOutput/service/ProductBoughtServiceImpl.java
+++ src/main/java/com/charann/JsonOutput/service/ProductBoughtServiceImpl.java
@@ -5,6 +5,7 @@ import com.charann.JsonOutput.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 @Service
@@ -27,6 +28,18 @@ public class ProductBoughtServiceImpl implements ProductBoughtService{
 
     double total_price = 0;
 
+    private static final DecimalFormat decfor = new DecimalFormat("0.00");
+
+    public String weight_convert(String unit) {
+
+        if(unit.equals("g")) {
+            return "kg";
+        } else if (unit.equals("ml")) {
+            return "L";
+        }
+        else return "";
+    }
+
     @Override
     public List<ProductBought> getAllProducts() {
         System.out.println(sumOfAmount());
@@ -36,12 +49,14 @@ public class ProductBoughtServiceImpl implements ProductBoughtService{
     // No.of products input to the store
     @Override
     public ProductBought createProduct(ProductBought productBought) {
+        System.out.println("SKU" +productBought.getSku());
 
-        if(productBoughtRepo.existsByName(productBought.getName())) {
+        if(productBoughtRepo.existsBySku(productBought.getSku())) {
             TotalBought totalBought = totalBoughtRepo.findOneByName(productBought.getName());
             totalBought.setTotal_price(productBought.getPrice() *
                     (productBought.getQuantity()+totalBought.getTotal_quantity()));
             totalBought.setTotal_quantity(totalBought.getTotal_quantity()+productBought.getQuantity());
+
             totalBoughtRepo.save(totalBought);
             System.out.println("Already exists");
 
@@ -49,8 +64,6 @@ public class ProductBoughtServiceImpl implements ProductBoughtService{
             RemainingStocks remainingStocks = remainingStocksRepo.findByName(productBought.getName());
             remainingStocks.setQuantity(remainingStocks.getQuantity()+productBought.getQuantity());
             remainingStocksRepo.save(remainingStocks);
-
-
         } else {
             TotalBought totalBought = new TotalBought();
             totalBought.setName(productBought.getName());
@@ -82,16 +95,18 @@ public class ProductBoughtServiceImpl implements ProductBoughtService{
         productBought1.setQuantity(productBought.getQuantity());
         productBought1.setPrice(productBought.getPrice());
         productBought1.setMonth(productBought.getMonth());
-
+        productBought1.setExpirydate(productBought.getExpirydate());
+        productBought1.setCategory(productBought.getCategory());
+        productBought1.setSku(productBought.getSku());
+        if(productBought.getWeight()>=1000) {
+            productBought1.setWeight(productBought.getWeight()/1000);
+            productBought1.setUnit(weight_convert(productBought.getUnit()));
+        } else {
+            productBought1.setWeight(productBought.getWeight());
+            productBought1.setUnit(productBought.getUnit());
+        }
         return productBoughtRepo.save(productBought1);
     }
-
-//    @Override
-//    public List<ProductBought> categorizeProducts() {
-//        System.out.println(productBoughtRepo.findCategorizedCount());
-//        return productBoughtRepo.findCategorizedCount();
-//    }
-
 
     @Override
     public double sumOfAmount() {
@@ -116,38 +131,22 @@ public class ProductBoughtServiceImpl implements ProductBoughtService{
 
         List<TotalBought> total_bought_list = totalBoughtRepo.findAll();
         total_bought_list.forEach((p)->{
+//            if(boughPieChartRepo.existsByName(p.getName())) {
+//                BoughtPieChart boughtPieChart = boughPieChartRepo.findByName(p.getName());
+//                boughtPieChart.setDegree(Double.parseDouble(decfor.format((
+//                        (p.getTotal_price()+boughtPieChart.getDegree())/total_amt)*360)));
+//                boughPieChartRepo.save(boughtPieChart);
+//            }
+//            else {
             BoughtPieChart boughtPieChart = new BoughtPieChart();
             boughtPieChart.setName(p.getName());
-            boughtPieChart.setDegree((p.getTotal_price()/total_amt)*360);
-            System.out.println((p.getTotal_price()/total_amt)*360);
+            boughtPieChart.setDegree(Double.parseDouble(decfor.format((p.getTotal_price() / total_amt) * 360)));
+            System.out.println((p.getTotal_price() / total_amt) * 360);
             boughPieChartRepo.save(boughtPieChart);
-            System.out.println("Name: "+boughtPieChart.getName()+"\nDegree: "+boughtPieChart.getDegree());
-        });
-
-        return "Success";
-
-    }
-
-//    @Override
-//    public String getMonthlyBarGraphData() {
-//        List<ProductBought> productBought = productBoughtRepo.findAll();
-//        productBought.forEach((p)-> {
-//
-//            if(!boughtBarGraphRepo.existsByMonth(p.getMonth())) {
-//                BoughtBarGraph boughtBarGraph = new BoughtBarGraph();
-//                boughtBarGraph.setBought_month(p.getMonth());
-//                boughtBarGraph.setBought_month_amount();
+            System.out.println("Name: " + boughtPieChart.getName() + "\nDegree: " + boughtPieChart.getDegree());
 //            }
-//        });
-//
-//        return "";
-//    }
-//
-//    @Override
-//    public double getMonthlyBoughtAmount() {
-//
-//        return 0;
-//    }
-
+        });
+        return "Success";
+    }
 
 }

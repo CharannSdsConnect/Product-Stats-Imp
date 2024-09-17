45630d8e9dc77c9f7aaabf80f5289a2c26e536fe
CharannSdsConnect <sdsuser010@gmail.com>
2024-09-17 15:51:36 +0530

    Update ProductSoldService.java

--- src/main/java/com/charann/JsonOutput/service/ProductSoldService.java
+++ src/main/java/com/charann/JsonOutput/service/ProductSoldService.java
@@ -3,15 +3,17 @@ package com.charann.JsonOutput.service;
 import com.charann.JsonOutput.entity.MonthlySales;
 import com.charann.JsonOutput.entity.ProductSold;
 import com.charann.JsonOutput.entity.TotalSales;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductSoldService {
     Optional<ProductSold> getProductById(Long id);
 
-    String addSoldProduct(ProductSold productSold);
+    ProductSold addSoldProduct(ProductSold productSold);
 
     String monthlyReport();
 
@@ -21,4 +23,14 @@ public interface ProductSoldService {
 
     String yearlyReport();
 
+    String dailyReport();
+
+    ProductSold updateProductSold(String sku);
+
+    void generateMonthlyReport(HttpServletResponse response) throws IOException;
+
+    void generateYearlyReport(HttpServletResponse response) throws IOException;
+
+    void generateDailyReport(HttpServletResponse response) throws IOException;
 }
+
